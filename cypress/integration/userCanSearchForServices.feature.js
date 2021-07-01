a15a3cefd4b568a6066fb7380a04c3efd215cd3a
DavDan1 <david-danielyan@hotmail.com>
2021-07-01 12:08:36 +0200

    adds test for header

---
+++ cypress/integration/userCanSearchForServices.feature.js
@@ -0,0 +1,16 @@
+describe('User can search for services', () => {
+  describe('Successfully', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'https://kcsc-api.herokuapp.com/api/search?q=**', {
+        fixture: 'services.json',
+      });
+      cy.visit('/');
+    });
+    it('is expected to show search input', () => {
+      cy.get('[data-cy=header-container]').within(() => {
+        cy.get('[data-cy=search-input]').should('be.visible');
+        cy.get('[data-cy=search-btn]').click();
+      });
+    });
+  });
+});

5ac3e29f861f67ac75799f4808e10815579a36a5
DavDan1 <david-danielyan@hotmail.com>
2021-07-01 17:18:19 +0200

    refactors header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,24 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Header = () => {
-  const [text, setText] = useState('');
+  const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
 
-  const searchServices = async (services) => {
+  const searchServices = async (query) => {
     const response = await axios.post(
-      `https://kcsc-api.herokuapp.com/api/search?q=${services}`
+      `https://kcsc-api.herokuapp.com/api/search?q=${query}`
     );
     setResults(response.data.services);
   };
-  const onSubmit = (event) => {
-    searchServices(text);
-    setText('');
+  const search = () => {
+    searchServices(query);
+    setQuery('');
   };
 
-  const onChange = (event) => setText(event.target.value);
-  let resultList = results.map((result) => {
- 
+  const resultList = results.map((result) => {
     return (
-      <div data-cy='results-list'>
-        <p>{result.name}</p>
+      <div key={result.id} data-cy='results-list'>
+        <h3>{result.name}</h3>
         <p>{result.description}</p>
       </div>
     );
@@ -33,14 +31,14 @@ const Header = () => {
         <input
           data-cy='search-input'
           type='text'
-          placeholder='search'
-          onChange={onChange}
+          placeholder='search here'
+          onChange={(event) => setQuery(event.target.value)}
         />
-        <button data-cy='search-btn' type='submit' onClick={onSubmit}>
+        <button data-cy='search-btn' type='button' onClick={search}>
           Search
         </button>
         <div data-cy='results-container'>
-          <div >{resultList}</div>
+          <div>{resultList}</div>
         </div>
       </div>
     </>
